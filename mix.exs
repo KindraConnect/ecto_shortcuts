885e27282f35302945d69381dd62815355a97dea
allie.jones <alliejones42@gmail.com>
2025-05-26 16:20:42 -0700

    Fix mix.exs: properly define package function and update dependencies

--- mix.exs
+++ mix.exs
@@ -2,29 +2,33 @@ defmodule EctoShortcuts.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :ecto_shortcuts,
-     version: "0.1.6",
-     elixir: "~> 1.3",
-     elixirc_paths: elixirc_paths(Mix.env),
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps(),
-     description: "Ecto extension adding shortcuts for common usecases",
-     package: package,
-     name: "EctoShortcuts",
-     docs: [source_url: "https://github.com/MishaConway/ecto_shortcuts"]]
+    [
+      app: :ecto_shortcuts,
+      version: "0.1.6",
+      elixir: "~> 1.4",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      package: package(),
+      description: "A collection of Ecto query shortcuts",
+      name: "EctoShortcuts",
+      docs: [source_url: "https://github.com/KindraConnect/ecto_shortcuts"]
+    ]
   end
 
   defp package do
-   [contributors: ["Misha Conway"],
-    licenses: ["MIT"],
-    links: %{github: "https://github.com/MishaConway/ecto_shortcuts"},
-    maintainers: ["Misha Conway"],
-    files: ~w(mix.exs README.md CHANGELOG.md lib)]
+    [
+      name: :ecto_shortcuts,
+      files: ~w(mix.exs README.md CHANGELOG.md lib),
+      maintainers: ["Misha Conway"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/KindraConnect/ecto_shortcuts"}
+    ]
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/lib", "test/migrations"]
-  defp elixirc_paths(_),     do: ["lib"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Configuration for the OTP application
   #
@@ -43,8 +47,10 @@ defmodule EctoShortcuts.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:ecto, ">= 2.0.4"},
-    {:ex_doc, ">= 0.0.0", only: :dev},
-    {:mariaex, ">= 0.0.0", only: :test}]
+    [
+      {:ecto, ">= 2.0.4"},
+      {:ex_doc, ">= 0.0.0", only: :dev},
+      {:mariaex, ">= 0.0.0", only: :test}
+    ]
   end
 end
